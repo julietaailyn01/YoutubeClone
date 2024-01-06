febc8aecdd6437fe19e9091057b685169b09a799
Julieta Ailyn Mosquera <julietaailyn@gmail.com>
2024-01-06 17:19:33 -0300

    06.1.2024

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -22,7 +22,7 @@ const Feed = () => {
         <SideBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-          Copyright © 2022 JSM Media
+          Copyright © 2024 JSM Media
         </Typography>
       </Box>
 
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { Box, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import Videos from './Videos'
+import { useParams } from 'react-router-dom';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const SearchFeed = () => {
+  const [videos, setVideos]= useState(null);
+  const {searchTerm } = useParams();
+
+  useEffect(()=>{
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+    .then((data) => setVideos(data.items))
+  }, [searchTerm]);
+  
   return (
-    <div>SearchFeed</div>
+    <Box>
+      <Typography>
+
+      </Typography>
+      <Box>
+        <Box/>
+        {<Videos videos={videos}/>}
+      </Box>
+    </Box>
   )
 }
 
